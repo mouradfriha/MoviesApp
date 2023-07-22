80bbbb5fc0f2d1006040053a4b93aadbef09c007
mouradfriha <mouradfriha1@gmail.com>
2023-07-22 23:04:05 +0100

    fix(header): ajouter les liens pour la creation du comptes et l'authentification

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,23 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
+
+        <div>
+            @if (Route::has('login'))
+                <div class="sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/bo/movies') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            
+        </div>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
