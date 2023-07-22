ba99cd4ebcfef965c2fa0b9340a0ec9b434f8640
mouradfriha <mouradfriha1@gmail.com>
2023-07-22 22:56:31 +0100

    feat(homepage): afficher la liste des films dans la homepage

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/movies', [MoviesController::class, 'index'])->name('movies.index');
+Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
 Route::get('/movies/{id}', [MoviesController::class, 'show'])->name('movies.show');
 
 
