0f2b9e590cdf7e186cd09d3540189c01d1f93047
mouradfriha <mouradfriha1@gmail.com>
2023-07-24 00:26:48 +0100

    ajoute une bouton acceuil au back office

--- resources/views/bo/movies.blade.php
+++ resources/views/bo/movies.blade.php
@@ -1,7 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
+        
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Liste des films') }}
+        </h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{-- <a href="{{route('movies.index')}}">  accueil </a> --}}
         </h2>
     </x-slot>
 
